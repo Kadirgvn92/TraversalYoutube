bdef52d0da4d50a13d02d3c0f34a047b595648d5
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-01-01 16:22:21 +0300

    Lesson 11 : EF Classes created

---
+++ TraversalYoutube.DataAccessLayer/EntityFramework/EfTestimonailDal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TraversalYoutube.DataAccessLayer.Abstract;
+using TraversalYoutube.DataAccessLayer.Repository;
+using TraversalYoutube.EntityLayer.Concrete;
+
+namespace TraversalYoutube.DataAccessLayer.EntityFramework;
+public class EfTestimonailDal : GenericRepository<Testimonail>, ITestimonailDal
+{
+}
