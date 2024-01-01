fe3f5538e4404684dc13d4c68a39227dff27e9de
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-01-01 17:32:15 +0300

    Lesson 14 - 1 : PresentationLayer started to created

---
+++ TraversalYoutube.PresentationLayer/Controllers/DefaultController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalYoutube.PresentationLayer.Controllers;
+public class DefaultController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
