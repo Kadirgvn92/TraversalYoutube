65c0158d3b3c1af746e95e02e73772eaa11a95aa
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-01-01 16:03:49 +0300

    Lesson 9 : IGenericDal created

---
+++ TraversalYoutube.DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TraversalYoutube.DataAccessLayer.Abstract;
+public interface IGenericDal<T> where T : class
+{
+    void Insert(T t);
+    void Update(T t);   
+    void Delete(T t);
+    void GetByID(int id);
+    List<T> GetAll();   
+}
