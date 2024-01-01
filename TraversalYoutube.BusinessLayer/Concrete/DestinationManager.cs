e1cdb382d5a2cbe3ca429b41703bb3240508717d
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-01-01 23:42:58 +0300

    Lesson 16 : PopularDestinatons Layout completed

---
+++ TraversalYoutube.BusinessLayer/Concrete/DestinationManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TraversalYoutube.BusinessLayer.Abstract;
+using TraversalYoutube.DataAccessLayer.Abstract;
+using TraversalYoutube.EntityLayer.Concrete;
+
+namespace TraversalYoutube.BusinessLayer.Concrete;
+public class DestinationManager : IDestinationService
+{
+    private readonly IDestinationDal _destinationDal;
+
+    public DestinationManager(IDestinationDal destinationDal)
+    {
+        _destinationDal = destinationDal;
+    }
+
+    public void TAdd(Destination entity)
+    {
+       _destinationDal.Insert(entity);
+    }
+
+    public void TDelete(Destination entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Destination> TGetAll()
+    {
+        return _destinationDal.GetAll();
+    }
+
+    public Destination TGetByID(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void TUpdate(Destination entity)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ TraversalYoutube.PresentationLayer/ViewComponents/Default/_PopularDestinationsPartial.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TraversalYoutube.DataAccessLayer.EntityFramework;
+using TraversalYoutube.EntityLayer.Concrete;
+using TraversalYoutube.BusinessLayer.Concrete;
+
+namespace TraversalYoutube.PresentationLayer.ViewComponents.Default;
+
+public class _PopularDestinationsPartial :ViewComponent
+{
+    DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
+    public IViewComponentResult Invoke()
+    {
+        var values = destinationManager.TGetAll();
+        return View(values);  
+    }
+}
