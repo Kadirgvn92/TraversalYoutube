18827510cce97f2888b7cc1dd09ca7c7f8c418d7
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-01-04 18:34:45 +0300

    Lesson 19 : All dynamic layout completed

--- TraversalYoutube.PresentationLayer/ViewComponents/Default/_TestimonailPartial.cs
+++ TraversalYoutube.PresentationLayer/ViewComponents/Default/_TestimonailPartial.cs
@@ -1,11 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TraversalYoutube.BusinessLayer.Concrete;
+using TraversalYoutube.DataAccessLayer.EntityFramework;
 
 namespace TraversalYoutube.PresentationLayer.ViewComponents.Default;
 
 public class _TestimonailPartial : ViewComponent
 {
+    TestimonailManager testimonailManager = new TestimonailManager(new EfTestimonailDal());
     public IViewComponentResult Invoke()
     {
-        return View();
+        var values = testimonailManager.TGetAll();
+        return View(values);
     }
 }
