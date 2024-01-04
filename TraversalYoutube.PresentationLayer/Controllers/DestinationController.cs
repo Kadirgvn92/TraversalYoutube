8d8fb5c6e225e1fea0eab74cd649866e5d1c2f42
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-01-04 19:08:12 +0300

    Lesson 20 : Destination Details page designed and made dynamic

---
+++ TraversalYoutube.PresentationLayer/Controllers/DestinationController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TraversalYoutube.BusinessLayer.Concrete;
+using TraversalYoutube.DataAccessLayer.EntityFramework;
+
+namespace TraversalYoutube.PresentationLayer.Controllers;
+public class DestinationController : Controller
+{
+    DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
+    public IActionResult Index()
+    {
+        var values = destinationManager.TGetAll();
+        return View(values);
+    }
+}
