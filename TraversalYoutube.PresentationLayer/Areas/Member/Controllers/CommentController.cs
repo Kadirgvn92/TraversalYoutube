b7ecadd733a24d93450b5871a799e835cccd72f8
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-01-07 18:10:31 +0300

    Lesson 32 : Area Member created and AdminLayout designed

---
+++ TraversalYoutube.PresentationLayer/Areas/Member/Controllers/CommentController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalYoutube.PresentationLayer.Areas.Member.Controllers;
+public class CommentController : Controller
+{
+    [Area("Member")]
+    [AllowAnonymous]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ TraversalYoutube.PresentationLayer/Areas/Member/Controllers/ProfileController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalYoutube.PresentationLayer.Areas.Member.Controllers;
+public class ProfileController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
