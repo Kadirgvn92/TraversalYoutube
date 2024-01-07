0237f2c47ab55e9253b92553a2936fe0ed4e8d95
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-01-07 20:00:29 +0300

    Lesson 33 : MemberLayout prepared and Destination admin completed

---
+++ TraversalYoutube.PresentationLayer/Areas/Member/Controllers/DestinationController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TraversalYoutube.BusinessLayer.Concrete;
+using TraversalYoutube.DataAccessLayer.EntityFramework;
+
+namespace TraversalYoutube.PresentationLayer.Areas.Member.Controllers;
+
+[Area("Member")]
+[AllowAnonymous]
+public class DestinationController : Controller
+{
+    DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
+
+    public IActionResult Index()
+    {
+        var values = destinationManager.TGetAll();
+        return View(values);
+    }
+}
