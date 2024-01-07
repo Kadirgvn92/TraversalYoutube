4e319bb78437d8d91084987beb95b3e17110aaf3
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-01-07 12:43:05 +0300

    Lesson 28 : Login and Register Pages downloaded and designed

---
+++ TraversalYoutube.PresentationLayer/Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalYoutube.PresentationLayer.Controllers;
+
+[AllowAnonymous]
+public class LoginController : Controller
+{
+    [HttpGet]
+    public IActionResult SignUp()
+    {
+        return View();
+    }
+    //[HttpPost]
+    //public IActionResult SignUp()
+    //{
+    //    return View();
+    //}
+    [HttpGet]
+    public IActionResult SignIn()
+    {
+        return View();
+    }
+}
