2f42df9c2c423bc6389545d08bdfc3c761250912
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-01-09 14:54:26 +0300

    Lesson 45 : Profile page revision made

---
+++ TraversalYoutube.BusinessLayer/Concrete/GuideManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TraversalYoutube.BusinessLayer.Abstract;
+using TraversalYoutube.DataAccessLayer.Abstract;
+using TraversalYoutube.EntityLayer.Concrete;
+
+namespace TraversalYoutube.BusinessLayer.Concrete;
+public class GuideManager : IGuideService
+{
+    private readonly IGuideDal _guideDal;
+
+    public GuideManager(IGuideDal guideDal)
+    {
+        _guideDal = guideDal;
+    }
+
+    public void TAdd(Guide entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void TDelete(Guide entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Guide> TGetAll()
+    {
+       return _guideDal.GetAll();
+    }
+
+    public Guide TGetByID(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void TUpdate(Guide entity)
+    {
+        throw new NotImplementedException();
+    }
+}
