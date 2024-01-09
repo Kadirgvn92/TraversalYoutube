eab9da02b508cc7520aff2dae8d082b25bcb17b5
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-01-09 14:02:42 +0300

    Lesson 43 : Dashboard created and designed

---
+++ TraversalYoutube.PresentationLayer/Areas/Member/Controllers/DashboardController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TraversalYoutube.EntityLayer.Concrete;
+
+namespace TraversalYoutube.PresentationLayer.Areas.Member.Controllers;
+
+[Area("Member")]
+public class DashboardController : Controller
+{
+    private readonly UserManager<AppUser> _userManager;
+
+    public DashboardController(UserManager<AppUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var values = await _userManager.FindByNameAsync(User.Identity.Name);
+        return View(values);
+    }
+}
