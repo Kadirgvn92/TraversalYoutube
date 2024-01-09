ca1e19e8e9a919c5724793c8b05204d2b3dedbcc
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-01-09 16:31:42 +0300

    Lesson 47 : Admin layout desinged and admin area created

---
+++ TraversalYoutube.PresentationLayer/Areas/Admin/Controllers/DestinationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalYoutube.PresentationLayer.Areas.Admin.Controllers;
+
+[Area("Admin")]
+public class DestinationController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
