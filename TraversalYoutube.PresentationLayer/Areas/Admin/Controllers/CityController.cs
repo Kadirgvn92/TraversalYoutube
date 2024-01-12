76e9d14d7e1ee7ee6de35f99006d9a490cdb00de
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-01-12 15:22:16 +0300

    Lesson 63 : Ajax library has been tested

---
+++ TraversalYoutube.PresentationLayer/Areas/Admin/Controllers/CityController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using TraversalYoutube.PresentationLayer.Models;
+
+namespace TraversalYoutube.PresentationLayer.Areas.Admin.Controllers;
+[Area("Admin")]
+public class CityController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult CityList()
+    {
+        var jsonCity = JsonConvert.SerializeObject(cities);
+        return Json(jsonCity);
+    }
+
+    public static List<City> cities = new List<City>()
+    {
+        new City
+        {
+            CityID = 1,
+            CityName = "Üsküp",
+            CityCountry = "Makedonya"
+        },
+        new City
+        {
+            CityID = 2,
+            CityName = "Roma",
+            CityCountry = "İtalya"
+        },
+        new City
+        {
+            CityID = 3,
+            CityName = "Londra",
+            CityCountry = "İngiltere"
+        }
+    };
+}
---
+++ TraversalYoutube.PresentationLayer/Models/City.cs
@@ -0,0 +1,8 @@
+﻿namespace TraversalYoutube.PresentationLayer.Models;
+
+public class City
+{
+    public int CityID { get; set; }
+    public string CityName { get; set; }
+    public string CityCountry { get; set; }
+}
