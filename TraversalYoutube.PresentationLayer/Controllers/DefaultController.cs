4fb4b7cf241da3cc841411db315bd7a39f25d8f3
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-01-17 14:40:24 +0300

    Lesson 77 : Refactoring process has been done and to be contined

--- TraversalYoutube.PresentationLayer/Controllers/DefaultController.cs
+++ TraversalYoutube.PresentationLayer/Controllers/DefaultController.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Mvc;
 namespace TraversalYoutube.PresentationLayer.Controllers;
 public class DefaultController : Controller
 {
-    
     public IActionResult Index()
     {
         return View();
